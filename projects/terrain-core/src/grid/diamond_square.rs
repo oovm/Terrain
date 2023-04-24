086f236ede28868beff01c732f94bdcaeed11195
Aster <aster@vers.site>
2023-04-24 20:59:51 +0800

    Record iteration parameters

--- projects/terrain-core/src/grid/diamond_square.rs
+++ projects/terrain-core/src/grid/diamond_square.rs
@@ -5,8 +5,9 @@ pub struct DiamondSquare {
     pub width: usize,
     pub height: usize,
     pub iteration: u32,
-    pub seed: u64,
     pub range: Range<f32>,
+    pub roughness: f32,
+    pub seed: u64,
 }
 
 impl Default for DiamondSquare {
@@ -16,6 +17,7 @@ impl Default for DiamondSquare {
             height: 4,
             iteration: 2,
             seed: 0,
+            roughness: 1.1,
             range: Range {
                 start: -1.0,
                 end: 1.0,
@@ -27,70 +29,40 @@ impl Default for DiamondSquare {
 impl DiamondSquare {
     pub fn generate(&self) -> GridTerrain {
         let mut rng = SmallRng::seed_from_u64(self.seed);
-        let step = 2usize.pow(self.iteration);
+        let mut step = 2usize.pow(self.iteration);
         let grid_w = step * self.width;
         let grid_h = step * self.height;
         let mut grid = Array2::zeros((grid_w, grid_h));
         let mut range = self.range.clone();
-        for y in (0..grid_h).step_by(step) {
-            for x in (0..grid_w).step_by(step) {
+        for x in (0..grid_h).step_by(step) {
+            for y in (0..grid_w).step_by(step) {
                 let value = rng.gen_range(self.range.start..self.range.end);
                 range.start = range.start.min(value);
                 range.end = range.end.max(value);
                 grid[[x, y]] = value;
             }
         }
-        for iteration in (0..self.iteration).rev() {
-            let step_x = self.width * 2usize.pow(iteration);
-            let step_y = self.height * 2usize.pow(iteration);
-            println!("Iteration: {}, step_x: {}, step_y: {}", iteration, step_x, step_y);
+        for iteration in 0..self.iteration {
+            step /= 2;
+            println!("Iteration: {}, step_x: {}, step_y: {}", 1 + self.iteration - iteration.ilog2(), step, step);
             // Diamond step
-            for y in (step_y..grid_h).step_by(step_y) {
-                for x in (step_x..grid_w).step_by(step_x) {
-                    let tl = grid[[x - step_x, y - step_y]];
-                    let tr = grid[[x + step_x, y - step_y]];
-                    let bl = grid[[x - step_x, y + step_y]];
-                    let br = grid[[x + step_x, y + step_y]];
-
-                    let avg = (tl + tr + bl + br) / 4.0;
-                    let value = avg + rng.gen_range(self.range.start..self.range.end);
-
-                    grid[[x, y]] = value;
-                }
-            }
-            // Square step
-            for y in (0..grid_h).step_by(step_y) {
-                for x in (0..grid_w).step_by(step_x) {
-                    let mut sum = 0.0;
-                    let mut count = 0;
-
-                    if x >= step_x {
-                        sum += grid[[x - step_x, y]];
-                        count += 1;
-                    }
-
-                    if x + step_x < grid_w {
-                        sum += grid[[x + step_x, y]];
-                        count += 1;
-                    }
-
-                    if y >= step_y {
-                        sum += grid[[x, y - step_y]];
-                        count += 1;
-                    }
-
-                    if y + step_y < grid_h {
-                        sum += grid[[x, y + step_y]];
-                        count += 1;
-                    }
-
-                    let avg = sum / count as f32;
-                    let value = avg + rng.gen_range(self.range.start..self.range.end);
-
-                    grid[[x, y]] = value;
+            for i in (1..=grid_h).step_by(step).map(|i|i - 1) {
+                for j in (1..=grid_w).step_by(step).map(|j| j - 1) {
+                    let lu = grid[[i, j]];
+                    let ru = grid[[i, j + step]];
+                    let ld = grid[[i + step, j]];
+                    let rd = grid[[i + step, j + step]];
+                    let avg = (lu + ru + ld + rd) / 4.0;
+                    let value = avg + self.get_rough_rate(&mut rng);
+                    grid[[i + step / 2, j + step / 2]] = value;
                 }
             }
         }
         GridTerrain { grid, range }
+    }
+
+    pub fn get_rough_rate(&self, rng: &mut SmallRng) -> f32 {
+        let r_roughness = self.roughness.recip();
+        rng.gen_range(r_roughness..self.roughness)
     }
 }
--- projects/terrain-core/tests/grid/mod.rs
+++ projects/terrain-core/tests/grid/mod.rs
@@ -3,7 +3,7 @@ use terrain_core::DiamondSquare;
 #[test]
 fn test() {
     let mut ds = DiamondSquare::default();
-    ds.iteration = 0;
+    ds.iteration = 2;
     let grid = ds.generate();
     println!("{:?}", grid);
 }
